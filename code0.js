68a2d44ee6cf403782e754601eba0e3389034579
webjesus <116731263+webjesus@users.noreply.github.com>
2024-06-01 15:51:21 +0300

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,43 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2= [];
+gdjs.Untitled_32sceneCode.GDNewSprite3Objects1= [];
+gdjs.Untitled_32sceneCode.GDNewSprite3Objects2= [];
+gdjs.Untitled_32sceneCode.GDNewSprite2Objects1= [];
+gdjs.Untitled_32sceneCode.GDNewSprite2Objects2= [];
+
+
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
+{gdjs.playerAuthentication.displayAuthenticationBanner(runtimeScene);
+}}
+
+}
+
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSprite3Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSprite3Objects2.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSprite2Objects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSprite2Objects2.length = 0;
+
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
